b7036e781daf40938bc63867c6e068c3d33c4130
CodeWarrior-debug <jpjordan1987@gmail.com>
2023-11-23 16:43:14 -0500

    authguard added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AuthGuard } from './common/auth/auth.guard';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+// import { LoggerMiddleware } from './middleware/logger.middleware'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalGuards(new AuthGuard())
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
