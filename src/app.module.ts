420f22226fb746be40528f0d0b060d2460195496
CodeWarrior-debug <jpjordan1987@gmail.com>
2023-11-24 16:59:10 -0500

    railway edits

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,40 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LoggerMiddleware } from './common/logger/logger.middleware';
-import { LoggerService } from './logger.service';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { AuthGuard } from './common/auth/auth.guard';
-import { BrowserInterceptor } from './common/browser/browser.interceptor';
-import { TransformResponseInterceptor } from './common/transform-response/transform-response.interceptor';
-import { AllExceptionsFilter } from './common/all-exceptions/all-exceptions.filter';
-
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { LoggerMiddleware } from './common/logger/logger.middleware'
+import { LoggerService } from './logger.service'
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core'
+import { AuthGuard } from './common/auth/auth.guard'
+import { BrowserInterceptor } from './common/browser/browser.interceptor'
+import { TransformResponseInterceptor } from './common/transform-response/transform-response.interceptor'
+import { AllExceptionsFilter } from './common/all-exceptions/all-exceptions.filter'
+import { ConfigModule } from '@nestjs/config'
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
     LoggerService,
     {
       provide: APP_GUARD,
-      useClass: AuthGuard,
+      useClass: AuthGuard
     },
     {
       provide: APP_INTERCEPTOR,
-      useClass: BrowserInterceptor,
+      useClass: BrowserInterceptor
     },
     {
       provide: APP_INTERCEPTOR,
-      useClass: TransformResponseInterceptor,
+      useClass: TransformResponseInterceptor
     },
     {
       provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-  ],
+      useClass: AllExceptionsFilter
+    }
+  ]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(`*`);
+    consumer.apply(LoggerMiddleware).forRoutes(`*`)
   }
 }
