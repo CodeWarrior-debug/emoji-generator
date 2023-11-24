392d1a5b13a75d26f1428c3e6da5c131eedf7cef
CodeWarrior-debug <jpjordan1987@gmail.com>
2023-11-23 22:53:41 -0500

    middleware not applying

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,6 @@ import { LoggerMiddleware } from './common/logger/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(`*`);
+    consumer.apply(LoggerMiddleware).forRoutes(`*`)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { AuthGuard } from './common/auth/auth.guard';
+import { AuthGuard } from './common/auth/auth.guard'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { LoggerMiddleware } from './common/logger/logger.middleware'
+import { BrowserInterceptor } from './common/browser/browser.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // app.use(new LoggerMiddleware())
   app.useGlobalGuards(new AuthGuard())
+  app.useGlobalInterceptors(new BrowserInterceptor())
   await app.listen(3000)
 }
 bootstrap()
